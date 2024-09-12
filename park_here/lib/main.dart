dd7b3ee44c3b2c42196c4baa34201d0778319c62
sartorianaclara04 <105017287+sartorianaclara04@users.noreply.github.com>
2024-09-12 16:36:29 -0300

    Atualização da main.dart de acordo com telas inicio e cadastro

--- park_here/lib/main.dart
+++ park_here/lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:park_here/tela-login.dart';
+import 'package:park_here/inicio.dart';
+import 'package:park_here/cadastroPerfil.dart';
+import 'package:park_here/cadastroVeiculo.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const String _title = 'Park Here';
+  //static const String _title = 'Park Here';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Login());
+    return MaterialApp(
+      title: 'ParkHere',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StartScreen(),
+      routes: {
+        '/login': (context) => const Login(),
+        '/inicio': (context) => const StartScreen(),
+        '/cadastroPerfil': (context) => const RegisterScreen(),
+        '/cadastroVeiculo': (context) => const VehicleRegisterScreen(),
+      },
+    );
   }
+
+  /*@override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Login());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Inicio());
+  }*/
 }
